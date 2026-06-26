5cb304a938131802b82844e0a60041610c5d191d
pmehats <pauline.mehats@gmail.com>
2026-06-26 15:22:18 +0200

    Tests génériques

--- tests/temp.rs
+++ tests/temp.rs
@@ -1,23 +1,59 @@
 #[cfg(test)]
 mod tests {
+    use moseiik::main::{compute_mosaic, Options};
+    use image::io::Reader as ImageReader;
+    use image::RgbImage;
     #[test]
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     fn test_x86() {
-        // TODO
-        // test avx2 or sse2 if available
-        assert!(false);
+        let opt = Options{
+            image: "assets/kit.png".to_string(),
+            output: "tests/kit_test.png".to_string(),
+            tiles: "assets/images".to_string(),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: true,
+            num_thread: 1,
+        };
+        let reference : RgbImage = ImageReader::open("assets/ground-truth-kit.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        let output : RgbImage = ImageReader::open("tests/kit_test.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        assert_eq!(reference,output);
     }
-
     #[test]
     #[cfg(target_arch = "aarch64")]
     fn test_aarch64() {
-        //TODO
-        assert!(false);
+        let opt = Options{
+            image: "assets/kit.png".to_string(),
+            output: "tests/kit_test.png".to_string(),
+            tiles: "assets/images".to_string(),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: true,
+            num_thread: 1,
+        };
+        let reference : RgbImage = ImageReader::open("assets/ground-truth-kit.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        let output : RgbImage = ImageReader::open("tests/kit_test.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        assert_eq!(reference,output);
     }
-
     #[test]
     fn test_generic() {
-        //TODO
-        assert!(false);
+        let opt = Options{
+            image: "assets/kit.png".to_string(),
+            output: "tests/kit_test.png".to_string(),
+            tiles: "assets/images".to_string(),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: false,
+            num_thread: 1,
+        };
+        let reference : RgbImage = ImageReader::open("assets/ground-truth-kit.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        let output : RgbImage = ImageReader::open("tests/kit_test.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        assert_eq!(reference,output);
     }
 }
